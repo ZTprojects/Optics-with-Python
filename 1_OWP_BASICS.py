35da0c5cd1b6b79fd8e28c4a0d4f3f486e780bbc
zomborit99-dotcom <zombori.t.99@gmail.com>
2026-06-29 17:10:46 +0200

    Update 1_OWP_BASICS.py

--- 1_OWP_BASICS.py
+++ 1_OWP_BASICS.py
@@ -8,6 +8,7 @@
 f, ob = 5, -6
 im = (f*ob) / (ob+f)
 print("Image distance: {0:.2f}".format(im))
+#Image distance: 30.00
 
 
 
@@ -20,6 +21,11 @@ t = 0
 while t<5:
     t=t+1
     print(t)
+""" 1
+    2
+    3
+    4
+    5"""
 
 
 name = 'Python'
@@ -27,13 +33,23 @@ i = 0
 while i < len(name):
     print(name[i])
     i+=1
-    
+""" P
+    y
+    t
+    h
+    o
+    n"""
     
 """for loop"""
 name = 'Python'
 for i in name:
     print(i)
-    
+""" P
+    y
+    t
+    h
+    o
+    n"""   
 
 
 
@@ -41,14 +57,14 @@ for i in name:
 
 
 """(2)_<<<<<<<<<<<<<<<<<<<<<<<<_INPUT FUNCTION_>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"""
-a = input('Wavelength: ')
-print(a)
-print(type(a))
+a = input('Wavelength: ')   #Wavelength:
+print(a)                    #555 - this is a string!
+print(type(a))              #<class 'str'> - I told you.
 
 
-a = float(input('Wavelength: '))
-print(a)
-print(type(a))
+a = float(input('Wavelength: '))    #Wavelength:
+print(a)                            #444.0 - this is a float!
+print(type(a))                      #<class 'float'>
 
 
 
@@ -60,14 +76,16 @@ print(type(a))
 def cube(x):
     #return the cube of a number
     return x**3
-
+    #'x' exists only in the "little bubble" of this function. It does not exist
+    #in the outside world. 'x' is shy.
 
 def volume(x):
     #calculates the volume of a sphere
     return 4 * 3.1415 * cube(x) / 3
+    #Here I summon a previously defined function: cube(). The 'cube' function 
+    #will take the variable from the 'volume' function
 
-
-r = input('Radius of sphere: ')
+r = input('Radius of sphere: ')     #Radius of sphere: 3
 r = float(r)
 V = volume(r)
 print('The volume of the sphere: {0:.2f}.'.format(V))
